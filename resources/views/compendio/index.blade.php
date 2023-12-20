526bff0a31f66ae0f248a3c592fada446ac09e81
J David Cayo <jdavidcayo@gmail.com>
2023-12-20 01:54:49 -0300

    fix download link

--- resources/views/compendio/index.blade.php
+++ resources/views/compendio/index.blade.php
@@ -84,7 +84,7 @@
                                                     d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
                                             </svg>
                                         </a>
-                                        <a href="{{ "../" . $compendio->urlDocumento }}" download="{{ $compendio->titulo . ".pdf" }}">
+                                        <a href="{{ "../" . $compendio->urlDocumento }}" download>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
                                                 fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
                                                 <path
